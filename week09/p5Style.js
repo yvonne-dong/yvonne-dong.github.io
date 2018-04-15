47670b152c2c82930da5aecc05cae64d49bbba22
yvonne-dong <dongy987@newschool.edu>
2018-04-15 18:00:37 -0400

    week09

---
+++ week09/p5Style.js
@@ -0,0 +1,29 @@
+
+function titleStyle(){
+    title.style('font-family', 'Arial Black','Gadget', 'sans-serif');
+    title.style('width', '180px');
+    title.style('padding', '10px');
+    title.style('border', '5px solid blue');
+    title.style('color', '#383838');
+}
+
+
+function buttonStyle(){
+  	buttonElement.style('background-color', 'white');
+  	buttonElement.style('border', 'none');
+  	buttonElement.style('padding', '10px');
+    buttonElement.style('padding-right', '38px');
+  	buttonElement.style('text-align', 'center');
+  	buttonElement.style('font-size', '20px');
+  	buttonElement.style('color', 'blue');
+    buttonElement.style('border', '5px solid blue');
+	  buttonElement.style('font-family', 'Arial Black','Gadget', 'sans-serif');
+    buttonElement.style('cursor', 'pointer');
+}
+
+function imgStyle(){
+	  exhibitionImg.style('width', '50%');
+  	exhibitionImg.style('height', '50%');
+  	exhibitionImg.style('padding', '10px');
+  	exhibitionImg.style('border', '5px solid #383838');
+}
---
+++ week09/sketch.js
@@ -0,0 +1,66 @@
+
+var exhibition; //store data
+var introData; //store data for introduction
+var exhibitionNum; //exhibition id
+var buttonElement; //for generating random exhibition
+var exhibitionImg; //exhibition image
+var pageNum; //what page of the record
+var text; //exhibition name
+var title; //heading
+var introduction; //intro text
+var url; //url to exhibition page
+
+function setup(){
+	noCanvas();
+
+  	title = createElement('h2', 'RANDOM EXHIBITION EXPLORER');
+  	buttonElement = createButton('find exhibition!');
+  	buttonElement.mouseOver(highlight);
+  	buttonElement.mouseOut(unhighlight);
+  	buttonElement.mousePressed(buttonPressed);
+  	buttonStyle();
+	titleStyle();
+  	text = createP("JODI: OXO");
+  	introduction = createP("DATE: 1998-06-04 to 1998-08-16");
+  	textStyle();
+  	url = createP("https://www.harvardartmuseums.org/visit/exhibitions/5664");
+  	url.style('font-size', '15px');
+  	url.style('text-decoration', 'underline');
+  	url.mouseOver(highlight);
+  	url.mouseOut(unhighlight);
+  	exhibitionImg = createImg('https://ids.lib.harvard.edu/ids/view/437010373?height=675');
+  	imgStyle();
+  	
+}
+
+function highlight() {
+  this.style('background-color', 'blue');
+  this.style('color', 'white');
+  this.style('cursor', 'pointer');
+  this.style('transition-duration', '0.4s');
+}
+
+function unhighlight() {
+  this.style('background-color', '#FFF');
+  this.style('color', '#383838');
+  this.style('transition-duration', '0.4s');
+}
+
+function buttonPressed() {
+	page = int(random(1,42));
+	var loading = loadJSON('https://api.harvardartmuseums.org/exhibition?hasimage=1&page='
+		+page+'&apikey=506b01a0-40d2-11e8-9ec4-7fae965d6296', getExhibition);
+	console.log(exhibitionNum);
+	console.log(loading);
+}
+
+function getExhibition(data) {
+	exhibition = data;
+	exhibitionNum = int(random(9));
+	exhibitionImg.remove();
+	exhibitionImg = createImg(exhibition.records[exhibitionNum].primaryimageurl);
+	imgStyle();
+  	text.html(exhibition.records[exhibitionNum].title);
+  	introduction.html("DATE: " + exhibition.records[exhibitionNum].begindate + " to " + exhibition.records[exhibitionNum].enddate);
+  	url.html(exhibition.records[exhibitionNum].url);
+}
