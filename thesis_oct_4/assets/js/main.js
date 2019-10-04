87b459120e9422e7f57b32a26c7c5a0eef84b9dd
Yanwen Dong <dongy987@newschool.edu>
2019-10-04 03:22:55 -0400

    edited photo case

--- thesis_oct_4/assets/js/main.js
+++ thesis_oct_4/assets/js/main.js
@@ -1,7 +1,26 @@
+
 window.addEventListener('DOMContentLoaded', function () {
-    attachPhoto();
+    casePhoto();
 });
 
+function casePhoto(){
+    $(".section").append("<h1>Photographer - Please take a photo of your favorite artwork in the show</h1>");
+    $(".section").append("<div class='collection'></div>");
+    $(".section").append("<input id='originPhoto' type='file' accept='image/*' capture='camera' style='width: 0; height: 0; overflow: hidden;' />");
+    $(".section").append("<div class='buttons'></div>");
+    $(".buttons").append("<div id='takePhoto' class='button'>TAKE PHOTO</div>");
+    $(".buttons").append("<div id='addPhoto' class='button'>ADD TO COLLECTION</div>");
+    attachPhoto();
+
+    $('#addPhoto').on('click', function(){
+        // let val = $(".collection").val;
+        // console.log(val);
+        $(".collection").empty();
+        alert("Your photos have been added to the collection");
+    });
+}
+
+// reference: https://jsfiddle.net/jpadc82h/
 function attachPhoto() {
     $("#takePhoto").click(function () {
         $("#originPhoto").click();
@@ -10,15 +29,18 @@ function attachPhoto() {
     $("#originPhoto").change(function() {
         for (var i = 0; i < this.files.length; i++) {
             var reader = new FileReader();
-            reader.onload = imageIsLoaded;
+            reader.onload = photoTaken;
             reader.readAsDataURL(this.files[i]);
         }
     });
 
-    function imageIsLoaded(e) {
-        // var x = 'foo';
-        var picture = '<img src="' + e.target.result + '" style="width:200px;" class="userPhoto">'
+    function photoTaken(e) {
+        var picture = '<img src="' + e.target.result + '" class="userPhoto">'
         $(".collection").append(picture);
     }
-
 }
+
+
+
+
+
