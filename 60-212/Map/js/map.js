0799ec196aa9378457cb9a3c6d8533319c8bfd51
Yanwen Dong <dongy987@newschool.edu>
2020-09-02 14:19:48 -0400

    added map

--- 60-212/Map/js/map.js
+++ 60-212/Map/js/map.js
@@ -1,12 +1,24 @@
-// example: https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_dynamic.html
+// inspired by this example: https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_dynamic.html
 
 var canvasWidth = 640;
 var canvasHeight = 640;
+
+//terrain
+var terrainSize = 100;
+var terrainDetails = 10;
+
+//trees
+var coneRadius = 5; //1-5
+var bushRadius = 5;
+var coneHeight = 8; //3-10
+var coneDetails = 32;
+var treePos = []; //store tree positions
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, canvasWidth / canvasHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(canvasWidth, canvasHeight);
+renderer.setClearColor(0xffd980, 1);
 document.body.appendChild(renderer.domElement);
 
 var controls = new THREE.OrbitControls(camera);
@@ -14,69 +26,78 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.1;
 controls.screenSpacePanning = false;
 
-var treePosId = [];
-
+//get random number between min & max
 function getRandomNum(min, max) {
     var num = min + Math.floor(Math.random() * Math.floor(max));
     return num;
 }
 
-function drawObjects() {
+function drawTerrain() {
     scene.remove.apply(scene, scene.children);
-    var light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(0, 10, 0).normalize();
+    var light = new THREE.PointLight(0xffffff);
+    light.position.set(-100, 350, 10);
     scene.add(light);
 
-    var geometry = new THREE.PlaneBufferGeometry(100, 100, 10, 10);
-    geometry.rotateX(-Math.PI / 2);
-    var material = new THREE.MeshPhongMaterial({ color: 0x452500, side: THREE.DoubleSide });
-    var terrain = new THREE.Mesh(geometry, material);
+    var terrainGeometry = new THREE.PlaneBufferGeometry(terrainSize, terrainSize, terrainDetails, terrainDetails);
 
-    var position = geometry.attributes.position;
+    terrainGeometry.rotateX(-Math.PI / 2);
+    var material = new THREE.MeshPhongMaterial({ color: 0x754a00, side: THREE.DoubleSide });
+    var terrain = new THREE.Mesh(terrainGeometry, material);
+
+    var position = terrainGeometry.attributes.position;
     position.usage = THREE.DynamicDrawUsage;
-
-    for (var j = 0; j < position.count; j++) {
-        treePosId.push(j);
-    }
-    // //reference: selecting elements from array without repeating https://jsfiddle.net/abdennour/oh3jL82j/
-    var randomize = treePosId.sort(function () { return .5 - Math.random() });
-    var treePosIdSelected = randomize.slice(0, 50);
-
-    var vector = new THREE.Vector3();
-
     for (var i = 0; i < position.count; i++) {
+        var vector = new THREE.Vector3();
         var height = getRandomNum(5, 10);
         var y = height * Math.random();
         position.setY(i, y);
-        var treeGeometry = new THREE.ConeGeometry(3, 8, 30);
-        var treeMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00, side: THREE.DoubleSide });
-        var cone = new THREE.Mesh(treeGeometry, treeMaterial);
-        vector.fromBufferAttribute(position, i);
-        // console.log(vect);
-        cone.position.set(vector.x, vector.y + 4, vector.z);
-        scene.add(cone);
-       
+        treePos.push(vector.fromBufferAttribute(position, i));
     }
-    var wireframe = new THREE.WireframeGeometry(geometry);
-    var line = new THREE.LineSegments(wireframe);
-    line.material.color.r = 0.16;
-    line.material.color.g = 0.08;
-    line.material.color.b = 0;
-    line.material.depthTest = false;
-    line.material.transparent = true;
-    scene.add(line);
-    scene.add(terrain);
 
+    //get random elements from array, reference: https://jsfiddle.net/abdennour/oh3jL82j/
+    var randomize1 = treePos.sort(function () { return .5 - Math.random() });
+    var conePos = randomize1.slice(0, 20);
+
+    var randomize2 = treePos.sort(function () { return .5 - Math.random() });
+    var bushPos = randomize2.slice(0, 20);
+
+    for (var j = 0; j < conePos.length; j++) {
+        coneRadius = getRandomNum(2, 5);
+        coneHeight = getRandomNum(4, 15);
+        bushRadius = getRandomNum(3, 7);
+        var coneGeometry = new THREE.ConeGeometry(coneRadius, coneHeight, coneDetails);
+        var coneMaterial = new THREE.MeshPhongMaterial({ color: 0x319c00, side: THREE.DoubleSide });
+        var cone = new THREE.Mesh(coneGeometry, coneMaterial);
+
+        var trunkGeometry = new THREE.CylinderGeometry(1, 1, 10, 32);
+        var trunkMaterial = new THREE.MeshPhongMaterial({ color: 0x240e00, side: THREE.DoubleSide });
+        var trunk1 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+        var trunk2 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+
+        var treeGeometry2 = new THREE.SphereGeometry(bushRadius, 5, 5);
+        var bushMaterial = new THREE.MeshPhongMaterial({ color: 0x42d100, side: THREE.DoubleSide });
+        var bush = new THREE.Mesh(treeGeometry2, bushMaterial);
+        
+        trunk1.position.set(conePos[j].x, conePos[j].y + 2.5, conePos[j].z);
+        trunk2.position.set(bushPos[j].x, bushPos[j].y + 2.5, bushPos[j].z);
+        scene.add(trunk1);
+        scene.add(trunk2);
+
+        cone.position.set(conePos[j].x, conePos[j].y + 7.5, conePos[j].z);
+        bush.position.set(bushPos[j].x, bushPos[j].y + 7.5, bushPos[j].z);        
+        scene.add(bush);
+        scene.add(cone);
+    }
+    scene.add(terrain);
 }
 
-drawObjects();
+drawTerrain();
 
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 32) {
-        // getRandomNum(randomObjectNum, 50, pos.length);
-        drawObjects();
+        drawTerrain();
     }
 }
 
